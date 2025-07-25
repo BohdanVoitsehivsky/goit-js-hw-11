8e446a23af2351709b6e11d957fe13dc88191ccf
BohdanVoitsehivsky <bogdanvoitsehivsky@gmail.com>
2025-07-25 13:05:09 +0300

    iziToast add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const API_KEY = "51465381-3669693f35fe6c0ef244a196a"
 const BASE_URL = 'https://pixabay.com/api/'
@@ -20,13 +20,13 @@ export function getImagesByQuery(query) {
     .then(res => {
         console.log(res);
         if(res.data.hits.length === 0) {
-//             iziToast.show({
-//     title: 'Ой, що це коїться???',
-//     message: 'Sorry, there are no images matching your search query. Please try again!',
-//     color: 'red',  
-//     position: 'topRight',
+            iziToast.show({
+    title: 'Ой, що це коїться???',
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    color: 'red',  
+    position: 'topRight',
 
-// });
+});
 return[];
         }
         return res.data.hits;
